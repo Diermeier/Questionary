f61d2c76d8fe8771bc483ecc793b90d11a6e0b37
diermeier <diermeier>
2019-01-26 18:12:35 +0100

    LevelBase

---
+++ app/src/main/java/com/rollnut/questionary/models/LevelBase.java
@@ -0,0 +1,22 @@
+package com.rollnut.questionary.models;
+
+public class LevelBase {
+
+    private int _levelNumber;
+    private String _myText;
+
+    public void setLevelNumber(int number)
+    {
+        _levelNumber = number;
+    }
+    public int get_levelNumber() {
+        return _levelNumber;
+    }
+
+    public void set_myText(String _myText) {
+        this._myText = _myText;
+    }
+    public String get_myText() {
+        return _myText;
+    }
+}
